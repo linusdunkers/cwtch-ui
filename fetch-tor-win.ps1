7d15c41aedaf8970898c81cde718d145a55108cb
Matthew van Eerde (^_^) <mateer@microsoft.com>
2021-08-26 10:41:06 -0700

    Pull the old version from the Cwtch archive rather than the new version from Tor directly

--- fetch-tor-win.ps1
+++ fetch-tor-win.ps1
@@ -1,6 +1,6 @@
 
-Invoke-WebRequest -Uri https://dist.torproject.org/torbrowser/10.5.5/tor-win64-0.4.5.10.zip -OutFile tor.zip
+Invoke-WebRequest -Uri https://git.openprivacy.ca/openprivacy/buildfiles/raw/branch/master/tor/tor-win64-0.4.6.5.zip -OutFile tor.zip
 
-if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne 'E5DA5899D9F4DDFBD33A8D4AB659659EBC1B47FDFE7BD27681D615792E6B9CB2EBA0BF381E8A7C8D89A3B011523786679883C4ECE492452F5F26E537149999D7' ) { Write-Error 'tor.zip sha512sum mismatch' }
+if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne '7917561a7a063440a1ddfa9cb544ab9ffd09de84cea3dd66e3cc9cd349dd9f85b74a522ec390d7a974bc19b424c4d53af60e57bbc47e763d13cab6a203c4592f' ) { Write-Error 'tor.zip sha512sum mismatch' }
 
 Expand-Archive -Path tor.zip -DestinationPath Tor
