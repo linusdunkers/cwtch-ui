f09c40d7dd7790d5b9741a62f016cd8e89365f09
Matthew van Eerde (^_^) <mateer@microsoft.com>
2021-08-26 09:51:03 -0700

    Bandaid fix - update the Tor tarball to fetch for Windows builds to a more recent version

--- fetch-tor-win.ps1
+++ fetch-tor-win.ps1
@@ -1,6 +1,6 @@
 
-Invoke-WebRequest -Uri https://dist.torproject.org/torbrowser/10.0.18/tor-win64-0.4.5.9.zip -OutFile tor.zip
+Invoke-WebRequest -Uri https://dist.torproject.org/torbrowser/10.5.5/tor-win64-0.4.5.10.zip -OutFile tor.zip
 
-if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne '72764eb07ad8ab511603aba0734951ca003989f5f4686af91ba220217b4a8a4bcc5f571b59f52c847932f6efedf847b111621983050fcddbb8099d43ca66fb07' ) { Write-Error 'tor.zip sha512sum mismatch' }
+if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne 'E5DA5899D9F4DDFBD33A8D4AB659659EBC1B47FDFE7BD27681D615792E6B9CB2EBA0BF381E8A7C8D89A3B011523786679883C4ECE492452F5F26E537149999D7' ) { Write-Error 'tor.zip sha512sum mismatch' }
 
 Expand-Archive -Path tor.zip -DestinationPath Tor
