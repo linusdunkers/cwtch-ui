fb930fb9ddbdab8958bc19956674d3d6689928d4
erinn <erinn@openprivacy.ca>
2022-02-02 15:42:36 -0800

    custom test params

--- run-tests.sh
+++ run-tests.sh
@@ -12,7 +12,7 @@ sed "s|featurePaths: REPLACED_BY_SCRIPT|featurePaths: <String>[$paths]|" integra
 flutter pub run build_runner clean
 flutter pub run build_runner build --delete-conflicting-outputs
 pkill tor
-LD_LIBRARY_PATH=/home/erinn/Android/Goprojects/libcwtch-go/ CWTCH_HOME=./integration_test/env/temp/ flutter drive --dart-define TEST_MODE=true --driver=test_driver/integration_test_driver.dart --target=integration_test/gherkin_suite_test.dart
+LD_LIBRARY_PATH=$LDPATH CWTCH_HOME=./integration_test/env/temp/ flutter drive --dart-define TEST_MODE=true --driver=test_driver/integration_test_driver.dart --target=integration_test/gherkin_suite_test.dart
 node index2.js
 if [ "$HEADLESS" = "false" ]; then
   xdg-open integration_test/gherkin/reports/cucumber_report.html
