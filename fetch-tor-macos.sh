16f413177fef5bcc92029e5cedb408387fd725d9
Dan Ballard <dan@mindstab.net>
2022-09-09 15:19:32 -0700

    bump tor versions, move linux to harvested tor

--- fetch-tor-macos.sh
+++ fetch-tor-macos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd macos
-curl https://git.openprivacy.ca/openprivacy/buildfiles/raw/branch/master/tor/tor-macos-0.4.6.7.tar.gz --output tor.tar.gz
+curl https://git.openprivacy.ca/openprivacy/buildfiles/raw/branch/master/tor/tor-macos-0.4.7.8.tar.gz --output tor.tar.gz
 tar -xzf tor.tar.gz
 chmod a+x Tor/tor.real
 cd ..
--- fetch-tor-win.ps1
+++ fetch-tor-win.ps1
@@ -1,6 +1,6 @@
 
-Invoke-WebRequest -Uri https://git.openprivacy.ca/openprivacy/buildfiles/raw/branch/master/tor/tor-win64-0.4.6.9.zip -OutFile tor.zip
+Invoke-WebRequest -Uri https://git.openprivacy.ca/openprivacy/buildfiles/raw/branch/master/tor/tor-win64-0.4.7.8.zip -OutFile tor.zip
 
-if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne 'bd99de56ef5ef9516410783ce48d52311093b26e718bf3d0a94efbd754d1cf2d12543f096139d9c289985349d26ee89b2308be5927fa1b410ff4f7f3129d6830' ) { Write-Error 'tor.zip sha512sum mismatch' }
+if ((Get-FileHash tor.zip -Algorithm sha512).Hash -ne '5b8f900a37f6e90d7a945b3903d769383c7478042cb43b2105d2374186e1a536f1a4758a2823d1d5be71d53a81dcfd8243293e04f82812d355983df322823cf4' ) { Write-Error 'tor.zip sha512sum mismatch' }
 
 Expand-Archive -Path tor.zip -DestinationPath Tor
