6abf506f18dc5a88891cd98f546869f6364c8376
Sarah Jamie Lewis <sarah@openprivacy.ca>
2022-12-28 18:51:49 -0800

    Error fetch lcg scripts on failure

--- fetch-libcwtch-go-macos.sh
+++ fetch-libcwtch-go-macos.sh
@@ -3,6 +3,6 @@
 VERSION=`cat LIBCWTCH-GO-MACOS.version`
 echo $VERSION
 
-curl https://build.openprivacy.ca/files/libCwtch-go-macos-$VERSION/libCwtch.x64.dylib --output libCwtch.x64.dylib
-curl https://build.openprivacy.ca/files/libCwtch-go-macos-$VERSION/libCwtch.arm64.dylib --output libCwtch.arm64.dylib
+curl --fail https://build.openprivacy.ca/files/libCwtch-go-macos-$VERSION/libCwtch.x64.dylib --output libCwtch.x64.dylib
+curl --fail https://build.openprivacy.ca/files/libCwtch-go-macos-$VERSION/libCwtch.arm64.dylib --output libCwtch.arm64.dylib
 
