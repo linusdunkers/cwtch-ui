029042c2fbc20887560c30f20bc34f555e06be08
Sarah Jamie Lewis <sarah@openprivacy.ca>
2023-04-05 11:08:36 -0700

    Package Onion Grater configs

--- linux/package-release.sh
+++ linux/package-release.sh
@@ -4,6 +4,7 @@ mv build/linux/x64/release/bundle/cwtch build/linux/x64/release/bundle/lib/cwtch
 cp linux/*.desktop build/linux/x64/release/bundle/
 cp linux/cwtch.*.sh build/linux/x64/release/bundle/
 cp linux/install-*.sh build/linux/x64/release/bundle/
+cp linux/cwtch-*.yml build/linux/x64/release/bundle/
 cp linux/cwtch build/linux/x64/release/bundle/
 cp README.md build/linux/x64/release/bundle/
 cp linux/cwtch.png build/linux/x64/release/bundle/
